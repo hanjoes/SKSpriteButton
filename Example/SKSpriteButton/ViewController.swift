6c4483e8a2e330ca2f73a3ffb24bcf1e346c0868
Hanzhou Shi <hanzhou87@gmail.com>
2017-10-01 17:52:29 -0700

    Initial commit

---
+++ Example/SKSpriteButton/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SKSpriteButton
+//
+//  Created by hanjoes on 10/01/2017.
+//  Copyright (c) 2017 hanjoes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
