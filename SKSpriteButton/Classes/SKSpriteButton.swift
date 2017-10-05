5cdcf8ff8de8a6dde9823c5bfa21fc99cb4925bf
Hanzhou Shi <hanzhou87@gmail.com>
2017-10-04 21:46:49 -0700

    Added documentation and missing "add" method for "touchesMoved" handler.

--- SKSpriteButton/Classes/SKSpriteButton.swift
+++ SKSpriteButton/Classes/SKSpriteButton.swift
@@ -3,27 +3,47 @@ import SpriteKit
 // MARK: SKSpriteButton
 
 /// (SeriousKit) SKSpriteButton.
-/// This is a special kind of `SKSpriteNode` that behaves like a button.
+/// This is a special kind of `SKSpriteNode` that behaves
+/// like a button.
+///
+/// - note: all `write` interaction with the object will
+/// set `isUserInteractionEnabled` to `true` so no need
+/// to explicitly set the variable.
 ///
 /// User should expect similar ergonomics when using `UIButton`.
 public class SKSpriteButton: SKSpriteNode {
     
+    /// Button status indicating the current state of the button.
+    ///
+    /// - normal: button is not tapped by user
+    /// - tapped: button is held down
     public enum Status {
         case normal
         case tapped
     }
     
+    /// The closure signature for the `event handler`.
+    /// Closure can be added to this button for specific events by calling
+    /// `add*` methods.
     public typealias EventHandler = (Set<UITouch>, UIEvent?) -> Void
 
     /// Set this variable if you want to display a
     /// different texture when the button is tapped.
-    public var tappedTexture: SKTexture?
+    public var tappedTexture: SKTexture? {
+        willSet {
+            isUserInteractionEnabled = true
+        }
+    }
     
     /// Color to display when a button is tapped.
-    public var tappedColor: UIColor?
+    public var tappedColor: UIColor? {
+        willSet {
+            isUserInteractionEnabled = true
+        }
+    }
     
-    /// Button status represented by `SKSpriteButton.Status`.
-    public var status: Status = .normal {
+    /// __Readonly__ button status represented by `SKSpriteButton.Status`.
+    private(set) public var status: Status = .normal {
         didSet {
             switch status {
             case .normal: showNormalAppearance()
@@ -44,19 +64,36 @@ public class SKSpriteButton: SKSpriteNode {
     
     internal var touchesMovedHandlers = [EventHandler]()
     
+    /// Add a method handler for `touchesBegan` event.
+    ///
+    /// - Parameter handler: a closure conforms to `SKSpriteButton.EventHandler`.
     public func addTouchesBeganHandler(handler: @escaping EventHandler) {
         isUserInteractionEnabled = true
         touchesBeganHandlers.append(handler)
     }
     
+    /// Add a method handler for `touchesEnded` event.
+    ///
+    /// - Parameter handler: a closure conforms to `SKSpriteButton.EventHandler`.
     public func addTouchesEndedHandler(handler: @escaping EventHandler) {
         isUserInteractionEnabled = true
         touchesEndedHandlers.append(handler)
     }
     
+    /// Add a method handler for `touchesCancelled` event.
+    ///
+    /// - Parameter handler: a closure conforms to `SKSpriteButton.EventHandler`.
     public func addTouchesCancelledHandler(handler: @escaping EventHandler) {
         isUserInteractionEnabled = true
         touchesCancelledHandlers.append(handler)
+    }
+    
+    /// Add a method handler for `touchesMoved` event.
+    ///
+    /// - Parameter handler: a closure conforms to `SKSpriteButton.EventHandler`.
+    public func addTouchesMovedHandler(handler: @escaping EventHandler) {
+        isUserInteractionEnabled = true
+        touchesMovedHandlers.append(handler)
     }
 }
 
